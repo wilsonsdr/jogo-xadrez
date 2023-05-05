54b7065c8271773b100c9f6bbe74faaebb7daf90
Wilson Reis <wilsonsdr@gmail.com>
2023-05-05 16:33:08 -0300

    feature: jogada especial promocao

--- xadrez-console/xadrez/PartidaXadrez.cs
+++ xadrez-console/xadrez/PartidaXadrez.cs
@@ -148,6 +148,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // PROMOCAO
+
+            if (p is Peao)
+            {
+                if (p.Cor == Cor.Branca && destino.Linha == 0 || p.Cor == Cor.Preta && destino.Linha == 7)
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversario(JogadorAtual)))
             {
                 Xeque = true;
@@ -166,8 +182,6 @@ namespace xadrez
                 Turno++;
                 MudarJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // # EN PASSANT
 
